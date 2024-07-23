2f86f0f5d93032e70de5c4f054d7b3c28c3e2498
thuvaragan-swivel <thuvaragan@swivelgroup.com.au>
2024-07-23 11:18:20 +0530

    server updates on express js.

--- express-server.js
+++ express-server.js
@@ -1,1 +1,28 @@
-console.log("Express JS...")
+console.log("Express JS...");
+
+import express from "express";
+import mongoose from "mongoose";
+import bodyParser from "body-parser";
+import dotenv from "dotenv";
+import expressRoute from "./routes/employee-route.js";
+
+// const express = require("express")
+const expressApp = express();
+
+expressApp.use(bodyParser.json());
+dotenv.config();
+
+const MONGO_URL = process.env.MONGODB_URL;
+const PORT = process.env.SERVER_PORT;
+
+mongoose
+  .connect(MONGO_URL)
+  .then(() => {
+    console.log("MongoDB Database has been connected successfully.");
+    expressApp.listen(PORT, () => {
+      console.log(`Express Server is running on Port ${PORT}.`);
+    });
+  })
+  .catch((error) => console.log(error));
+
+expressApp.use("/api", expressRoute);
