8029f8d40f83f78a1f83316d3f849cce2e043982
thuvaragan-swivel <thuvaragan@swivelgroup.com.au>
2024-08-07 20:23:38 +0530

    .

--- expressApp.js
+++ expressApp.js
@@ -20,9 +20,8 @@ const corsOptions = {
 
 expressApp.use(cors(corsOptions)); // Enabling CORS.
 expressApp.use(bodyParser.json()); // Parsing JSON bodies for incoming requests.
+expressApp.use("/api", expressRouter); // Using the routes defined in the crudRoutes.js file.
 
 expressApp.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs)); // Using Swagger for API Documentation.
-
-expressApp.use("/api", expressRouter); // Using the routes defined in the crudRoutes.js file.
 
 export default expressApp;
