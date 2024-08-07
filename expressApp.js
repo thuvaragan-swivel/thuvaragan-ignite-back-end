738766a68a24040a78bbb896739370b9ebbf5a8b
thuvaragan-swivel <thuvaragan@swivelgroup.com.au>
2024-08-07 17:11:29 +0530

    swagger css updates

--- expressApp.js
+++ expressApp.js
@@ -22,6 +22,12 @@ expressApp.use(cors(corsOptions)); // Enabling CORS.
 expressApp.use(bodyParser.json()); // Parsing JSON bodies for incoming requests.
 expressApp.use("/api", expressRouter); // Using the routes defined in the crudRoutes.js file.
 
-expressApp.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs)); // Using Swagger for API Documentation.
+const CSS_URL = 'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.1.0/swagger-ui.min.css';
+
+const options = {
+customCssUrl: CSS_URL,
+};
+
+expressApp.use('/api-docs', swaggerUi.serveFiles(swaggerDocs, options), swaggerUi.setup(swaggerDocs, options)); // Using Swagger for API Documentation.
 
 export default expressApp;
