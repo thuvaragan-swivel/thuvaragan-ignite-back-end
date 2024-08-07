5a52178019da590ab1d2db38930ca2f654acfa57
thuvaragan-swivel <thuvaragan@swivelgroup.com.au>
2024-08-07 15:08:56 +0530

    .

---
+++ api/expressServer.js
@@ -0,0 +1,21 @@
+import expressApp from "./expressApp.js";
+import connectDB from "./config/mongoConfig.js";
+import logger from "./config/loggerConfig.js";
+
+const PORT = process.env.SERVER_PORT; // Getting the server port.
+
+// Function to start the Express server.
+// It connects to the MongoDB database and then starts the server.
+const startExpressServer = async () => {
+  try {
+    logger.info(`Starting Express Server on Port ${PORT}.`);
+    await connectDB(); // Connecting to the MongoDB database.
+    expressApp.listen(PORT, () => {
+      logger.info(`Express Server is running on Port ${PORT}.\n`);
+    });
+  } catch (error) {
+    logger.error(`Failed to start server: ${error.message}.`);
+  }
+};
+
+startExpressServer();
