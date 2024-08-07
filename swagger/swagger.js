c31ed1937a3b85907aa4278d850ef0b187391dd7
thuvaragan-swivel <thuvaragan@swivelgroup.com.au>
2024-08-07 10:45:22 +0530

    swagger comments

--- swagger/swagger.js
+++ swagger/swagger.js
@@ -1,17 +1,16 @@
-// swagger.js
-import swaggerJsdoc from 'swagger-jsdoc';
-import swaggerUi from 'swagger-ui-express';
+import swaggerJsdoc from "swagger-jsdoc";
+import swaggerUi from "swagger-ui-express";
 
 const swaggerOptions = {
   definition: {
-    openapi: '3.0.0',
+    openapi: "3.0.0",
     info: {
-      title: 'Swagger Express API',
-      version: '1.0.0',
-      description: 'A Simple Express API with Swagger Documentation.',
+      title: "Swagger Express API",
+      version: "1.0.0",
+      description: "A Simple Express API with Swagger Documentation.",
     },
   },
-  apis: ['./routes/crudRoutes.js'], // Path to your API routes
+  apis: ["./routes/crudRoutes.js"], // Path to the API routes.
 };
 
 const swaggerDocs = swaggerJsdoc(swaggerOptions);
