2f0e7c60b9987333b813df4c2cfc2020e64dce53
thuvaragan-swivel <thuvaragan@swivelgroup.com.au>
2024-08-07 16:56:55 +0530

    error handling part updates

--- config/loggerConfig.js
+++ config/loggerConfig.js
@@ -1,25 +1,31 @@
 import dotenv from "dotenv";
-import winston from 'winston';
+import winston from "winston";
 
 dotenv.config();
 
 // Creating a Winston logger instance.
 const logger = winston.createLogger({
-  level: 'info', // Default log level.
+  level: "info", // Default log level.
   format: winston.format.combine(
     winston.format.timestamp(),
     winston.format.printf(({ level, message, timestamp }) => {
       return `${timestamp} - ${level.toUpperCase()}: ${message}`;
     })
   ),
-  transports: [
-    new winston.transports.Console()
-  ],
+  transports: [new winston.transports.Console()],
 });
 
 // Error handling for transport initialization.
-logger.on('error', (err) => {
-  console.error('Logger error:', err);
+logger.on("error", (err) => {
+  handleLoggerError(err);
 });
+
+const handleLoggerError = (err) => {
+  let loggerErrors = [];
+  loggerErrors.push({
+    message: `Logger error: ${err.message}`,
+    timestamp: new Date().toISOString(),
+  });
+};
 
 export default logger;
