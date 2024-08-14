d0b1adf191a21b30fd81702e05b9b55d2eeb6399
thuvaragan-swivel <thuvaragan@swivelgroup.com.au>
2024-08-14 16:08:57 +0530

    test updates

--- tests/model/employeeModel.test.js
+++ tests/model/employeeModel.test.js
@@ -1,7 +1,7 @@
 import Employee from "../../src/model/employeeModel.js";
 
 describe("Employee Model", () => {
-  it("should create an Employee schema", () => {
+  it("should create an Employee schema and exclude unwanted fields in JSON output", () => {
     const employee = new Employee({
       firstName: "Jonathan",
       lastName: "Davidson",
@@ -11,11 +11,20 @@ describe("Employee Model", () => {
       employeeId: 1,
     });
 
-    expect(employee).toHaveProperty("firstName", "Jonathan");
-    expect(employee).toHaveProperty("lastName", "Davidson");
-    expect(employee).toHaveProperty("emailAddress", "jon.dav@gmail.com");
-    expect(employee).toHaveProperty("phoneNumber", "+94123456789");
-    expect(employee).toHaveProperty("gender", "Male");
-    expect(employee).toHaveProperty("employeeId", 1);
+    // Converting to JSON to apply the toJSON transformation.
+    const employeeJson = employee.toJSON();
+
+    // Checking for the expected properties.
+    expect(employeeJson).toHaveProperty("firstName", "Jonathan");
+    expect(employeeJson).toHaveProperty("lastName", "Davidson");
+    expect(employeeJson).toHaveProperty("emailAddress", "jon.dav@gmail.com");
+    expect(employeeJson).toHaveProperty("phoneNumber", "+94123456789");
+    expect(employeeJson).toHaveProperty("gender", "Male");
+    expect(employeeJson).toHaveProperty("employeeId", 1);
+
+    // Ensuring that the unwanted fields are not present.
+    expect(employeeJson).not.toHaveProperty("_id");
+    expect(employeeJson).not.toHaveProperty("__v");
+    expect(employeeJson).not.toHaveProperty("updatedAt");
   });
 });
