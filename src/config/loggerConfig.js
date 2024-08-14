adb79601e4b8e4811109dfec6bcc3b6ddb1e3e95
thuvaragan-swivel <thuvaragan@swivelgroup.com.au>
2024-08-14 11:16:56 +0530

    logger updates

--- src/config/loggerConfig.js
+++ src/config/loggerConfig.js
@@ -4,34 +4,6 @@ import winston from "winston";
 dotenv.config();
 
 // Creating a Winston logger instance.
-// const logger = winston.createLogger({
-//   level: "info", // Default log level.
-//   format: winston.format.combine(
-//     winston.format.timestamp(),
-//     winston.format.printf(({ level, message, timestamp }) => {
-//       return `${timestamp} - ${level.toUpperCase()}: ${message}`;
-//     })
-//   ),
-//   transports: [
-//     new winston.transports.Console(),
-//     new winston.transports.File({ filename: "logs/back-end.log" }), // Log to a file
-//   ],
-// });
-
-// export default logger;
-
-const transports = [
-  new winston.transports.Console(),
-];
-
-// Adding file transport only in development mode.
-if (process.env.NODE_ENV === "development") {
-  transports.push(
-    new winston.transports.File({ filename: "logs/back-end.log" })
-  );
-}
-
-// Creating a Winston logger instance.
 const logger = winston.createLogger({
   level: "info", // Default log level.
   format: winston.format.combine(
@@ -40,7 +12,10 @@ const logger = winston.createLogger({
       return `${timestamp} - ${level.toUpperCase()}: ${message}`;
     })
   ),
-  transports, // Using the conditional transports array.
+  transports: [
+    // new winston.transports.Console(),
+    new winston.transports.File({ filename: "logs/back-end.log" }), // Log to a file
+  ],
 });
 
 export default logger;
