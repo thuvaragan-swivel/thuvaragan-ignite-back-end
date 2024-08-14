0d1568030d32e59c28e39472f5c27383a26f5cd4
thuvaragan-swivel <thuvaragan@swivelgroup.com.au>
2024-08-14 11:23:30 +0530

    logger updates

--- src/config/loggerConfig.js
+++ src/config/loggerConfig.js
@@ -13,8 +13,8 @@ const logger = winston.createLogger({
     })
   ),
   transports: [
-    // new winston.transports.Console(),
-    new winston.transports.File({ filename: "logs/back-end.log" }), // Log to a file
+    new winston.transports.Console(),
+    // new winston.transports.File({ filename: "logs/back-end.log" }), // Log to a file
   ],
 });
 
