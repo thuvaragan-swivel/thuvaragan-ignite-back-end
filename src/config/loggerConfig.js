8ca53404881b17afc4f3d07a6d620f2213b598a9
thuvaragan-swivel <thuvaragan@swivelgroup.com.au>
2024-08-15 15:10:58 +0530

    logger updates

--- src/config/loggerConfig.js
+++ src/config/loggerConfig.js
@@ -3,7 +3,16 @@ import winston from "winston";
 
 dotenv.config();
 
-// Creating a Winston logger instance.
+const transports = [
+  new winston.transports.Console(), // Logging to console.
+];
+
+// Conditionally adding file logging if not in a serverless environment.
+if (process.env.NODE_ENV !== "production" && process.env.VERCEL_ENV === undefined) {
+  transports.push(new winston.transports.File({ filename: "logs/back-end.log" }));
+} // This has been done, since Vercel deployment crashes when using external file logging, since the environment is serverless.
+  // In serverless environments, there is no persistent file system available, meaning, writing logs to files cannot be done as it could be on a traditional server.
+
 const logger = winston.createLogger({
   level: "info", // Default log level.
   format: winston.format.combine(
@@ -12,10 +21,7 @@ const logger = winston.createLogger({
       return `${timestamp} - ${level.toUpperCase()}: ${message}`;
     })
   ),
-  transports: [
-    new winston.transports.Console(),
-    // new winston.transports.File({ filename: "logs/back-end.log" }), // Log to a file
-  ],
+  transports,
 });
 
 export default logger;
